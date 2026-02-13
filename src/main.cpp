c06a99da72a6d8c473a01390b3cff570ec3a8e13
Mattias Dürrmeier <matt.duerr@pm.me>
2026-02-13 23:56:00 +0100

    Add square player

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
-
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
-int main ()
-{
+typedef struct Player {
+	Vector2 position;
+	Vector2 size;
+} Player;
+
+void checkKeyboardInput(Player& player) {
+	if (IsKeyDown(KEY_A)) player.position.x -= 5.0f;
+	if (IsKeyDown(KEY_D)) player.position.x += 5.0f;
+}
+
+int main () {
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	const int screenWidth = 1280;
+	const int screenHeight = 800;
+	InitWindow(screenWidth, screenHeight, "LVLUP Game Jam 2026");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
@@ -25,29 +26,29 @@ int main ()
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
+	Player player = { 0 };
+	player.position = {x: 50, y: screenHeight - 100 };
+	player.size = { 80, 80 };
 	// game loop
-	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
+	while (!WindowShouldClose()) {		// run the loop until the user presses ESCAPE or presses the Close button on the window
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
 		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		DrawText("Demo Text", 10,10,20,WHITE);
 
+		checkKeyboardInput(player);
+		DrawRectangleV(player.position, player.size, BLUE);
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		// DrawTextureV(wabbit, position, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
-
-	// cleanup
 	// unload our texture so it can be cleaned up
 	UnloadTexture(wabbit);
-
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
