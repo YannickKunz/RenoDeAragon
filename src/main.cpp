8a2fb992c88436165b5719e58f810188c34c1333
Mattias Dürrmeier <matt.duerr@pm.me>
2026-02-14 11:47:06 +0100

    Add gravity, jump and collision

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,56 @@
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
 #define TOGGLE_DELAY_SEC 2.0f
+#define G 800
+#define JUMP_SPEED 600.0f
+#define MOVEMENT 200.0f
 
 typedef struct Player {
 	Vector2 position;
 	Vector2 size;
+	float speed;
+	bool canJump;
 	bool toggle;
 	float toggleCooldown;
 } Player;
 
-void updatePlayer(Player& player) {
-	player.toggleCooldown += GetFrameTime(); // could this overflow?
-	if (IsKeyDown(KEY_A)) player.position.x -= 5.0f;
-	if (IsKeyDown(KEY_D)) player.position.x += 5.0f;
+void updatePlayer(Player& player, Rectangle (&platforms)[], int platformsLength) {
+	float delta = GetFrameTime();
+	player.toggleCooldown += delta; // could this overflow?
+	if (IsKeyDown(KEY_A)) player.position.x -= MOVEMENT*delta;
+	if (IsKeyDown(KEY_D)) player.position.x += MOVEMENT*delta;
 	if (IsKeyPressed(KEY_F) && (player.toggleCooldown >= TOGGLE_DELAY_SEC)) {
 		player.toggle = !player.toggle;
 		player.toggleCooldown = 0.0f;
 	}
+	if (IsKeyDown(KEY_SPACE) && player.canJump) {
+		player.speed -= JUMP_SPEED;
+		player.canJump = false;
+	}
 
-	DrawRectangleV(player.position, player.size, BLUE);
+	bool hitObstacle = false;
+	for (int i = 0; i < platformsLength; i++) {
+		Rectangle plat = platforms[i];
+		if (plat.x <= player.position.x && (plat.x + plat.width) >= player.position.x) {
+			if (plat.y >= player.position.y && plat.y <= (player.position.y + player.speed * delta)) {
+				hitObstacle = true;
+				player.speed = 0.0f;
+				player.position.y = plat.y;
+				break;
+			}
+		}
+	}
+
+	if (hitObstacle) {
+		player.canJump = true;
+	} else {
+		player.position.y += player.speed * delta;
+		player.speed += G * delta;
+		player.canJump = false;
+	}
+
+	Vector2 playerPosition = { player.position.x - player.size.x/2, player.position.y - player.size.y };
+	DrawRectangleV(playerPosition, player.size, BLUE);
 }
 
 int main () {
@@ -34,21 +66,22 @@ int main () {
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
-	
+
 	Player player = { 0 };
 	player.position = { 50, screenHeight - 100 };
 	player.size = { 50, 50 };
 	player.toggleCooldown = 2.0f;
 
 	Rectangle platforms[] = {
-		{200, 300, 100, 10},
-		{300, 400, 100, 10},
+		{400, 600, 100, 10},
+		{600, 500, 100, 10},
 		{0, screenHeight - 50, screenWidth, 50},
 	};
 
 	int platformsLength = sizeof(platforms)/sizeof(platforms[0]);
 
 	// game loop
+	SetTargetFPS(60);
 	while (!WindowShouldClose()) {		// run the loop until the user presses ESCAPE or presses the Close button on the window
 		BeginDrawing();
 
@@ -63,13 +96,12 @@ int main () {
 
 		DrawText(debugText.c_str(), 10, 10, 20, WHITE);
 
-		updatePlayer(player);
-
+		updatePlayer(player, platforms, platformsLength);
 		for (int i = 0; i < platformsLength; i++) {
 			Rectangle rec = platforms[i];
 			DrawRectangleRec(rec, GRAY);
 		}
-		
+
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
